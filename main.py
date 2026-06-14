ec9e0e6630606ca679a31784ef7f194c7c35dbb4
0xleonf <ucihasaskee07@gmail.com>
2026-06-14 08:13:32 +0800

    just to try my C library on python :)

---
+++ main.py
@@ -0,0 +1,13 @@
+import bulkifxApp as bp
+
+
+output_ = "gray_output.jpg"
+
+img = bp.load_image("patrick.jpg")
+
+img_gray = bp.convert_grayscale(img)
+
+bp.free_image(img)
+bp.save_image(output_, img_gray)
+
+bp.free_image(img_gray)
