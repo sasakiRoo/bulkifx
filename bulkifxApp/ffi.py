9c0f05f946fd5e3ad31af2de9692161713f3ab6f
leon <152042828+0xleonf@users.noreply.github.com>
2026-06-14 08:19:34 +0800

    Implement abi (#13)
    * feat: setup the ffi (foreign function interface) to interact the .so with python
    
    * just to try my C library on python :)
    
    * chore: setup all the function needed to easily use to import
    
    * chore: redefined the original function from C to python
    
    * it might be worst cmakelist
    
    * chore: add some file or dir to ignore

---
+++ bulkifxApp/ffi.py
@@ -0,0 +1,47 @@
+import ctypes
+import os
+import sys
+
+_curr_dir = os.path.dirname(os.path.abspath(__file__))
+
+_so_name = "libbulkifx.so"
+_path_so = os.path.join(_curr_dir, _so_name)
+
+if not os.path.exists(_path_so):
+    _path_so = os.path.abspath(os.path.join(_curr_dir, "../build", _so_name))
+
+if not os.path.exists(_path_so):
+    raise ImportError(
+        f"Binary {_path_so} not found"
+        f"make sure compiled C have done")
+
+lib = ctypes.CDLL(_path_so)
+
+
+class Image(ctypes.Structure):
+    _fields_ = [
+        ("width", ctypes.c_int),
+        ("height", ctypes.c_int),
+        ("channels", ctypes.c_int),
+        ("data", ctypes.POINTER(ctypes.c_uint8))
+    ]
+
+
+ImagePtr = ctypes.POINTER(Image)
+
+# core. c
+lib.load_image.argtypes = [ctypes.c_char_p]
+lib.load_image.restype = ImagePtr
+
+lib.image_print_info.argtypes = [ImagePtr]
+lib.image_print_info.restype = None
+
+lib.save_image.argtypes = [ctypes.c_char_p, ImagePtr]
+lib.save_image.restype = None
+
+lib.image_free.argtypes = [ImagePtr]
+lib.image_free.restype = None
+
+# img_proc.c
+lib.grayscale.argtypes = [ImagePtr]
+lib.grayscale.restype = ImagePtr
