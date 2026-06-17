fb2d1ab99029ef0b11c177f172afe774558109a2
leon <152042828+0xleonf@users.noreply.github.com>
2026-06-17 20:20:20 +0800

    Optimize computation (#18)
    * chore: use auto-vectorize on the compiler
    
    * chore: freed the image inside the save_image function
    
    * ignored
    
    * nothing special here
    
    * just try to use the lib in python
    
    * refactor: implement readable loop

--- main.c
+++ main.c
@@ -3,42 +3,42 @@
 #include <stdint.h>
 #include <stdio.h>
 
-// void check_matrices(Image *img, int target_channel, int region_size) {
-//   if (!img)
-//     return;
-//
-//   for (int y = 0; y < region_size; y++) {
-//     for (int x = 0; x < region_size; x++) {
-//
-//       uint8_t *pixel = get_pixel(img, x, y);
-//
-//       if (pixel != NULL) {
-//         printf("%3d ", pixel[target_channel]);
-//       } else {
-//         printf(" ? ");
-//       }
-//     }
-//     printf("\n");
-//   }
-//
-//   // uint8_t *pixel =get_pixel(img, 2, 2);
-//   // double hasil = pixel[1]*0.587;
-//   //
-//   // printf("%d\n", pixel[1]);
-//   // printf("%f\n", hasil);
-// }
+void check_matrices(Image *img, int target_channel, int region_size) {
+  if (!img)
+    return;
+
+  for (int y = 0; y < region_size; y++) {
+    for (int x = 0; x < region_size; x++) {
+
+      uint8_t *pixel = get_pixel(img, x, y);
+
+      if (pixel != NULL) {
+        printf("%3d ", pixel[target_channel]);
+      } else {
+        printf(" ? ");
+      }
+    }
+    printf("\n");
+  }
+
+  uint8_t *pixel = get_pixel(img, 2, 2);
+  double hasil = pixel[1] * 0.587;
+
+  printf("%d\n", pixel[1]);
+  printf("%f\n", hasil);
+}
 
 int main() {
-  // const char *filename = "patrick.jpg";
-  // Image *img = load_image(filename);
-  //
-  // Image *gray = grayscale(img);
-  //
-  // save_image("output_gray.png", gray);
+  const char *filename = "patrick.jpg";
+  Image *img = load_image(filename);
 
-  // image_print_info(img);
-  //
-  // check_matrices(img, 1, 10);
+  Image *gray = grayscale(img);
+
+  save_image("output_gray.jpg", gray);
+
+  image_print_info(img);
+
+  check_matrices(img, 0, 10);
   // printf("hello");
   return 0;
 }
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import bulkifxApp as bp
 
-
 output_ = "gray_output.jpg"
 
-img = bp.load_image("spon.jpg")
+img = bp.load_image("patrick.jpg")
 
 img_gray = bp.convert_grayscale(img)
 
-bp.free_image(img)
 bp.save_image(output_, img_gray)
-
-bp.free_image(img_gray)
