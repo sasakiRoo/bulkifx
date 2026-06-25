ae6fa6df427a8de7ba62a40d9a1f37a66be2b44f
0xleonf <ucihasaskee07@gmail.com>
2026-06-25 23:22:00 +0800

    refactor: remapping color channel

--- src/img_proc.c
+++ src/img_proc.c
@@ -5,30 +5,25 @@
 #include <stdlib.h>
 
 Image *grayscale(Image *img) {
-  if (!img->data)
+  if (!img || !img->R || !img->G || !img->B)
     return NULL;
 
   Image *gray_img = create_image(img->width, img->height, 1);
 
   int total_pixel = (img->width * img->height);
-  uint8_t *gray_pixel = (uint8_t *)malloc(total_pixel);
   uint8_t *red = (uint8_t *)malloc(total_pixel);
   uint8_t *green = (uint8_t *)malloc(total_pixel);
   uint8_t *blue = (uint8_t *)malloc(total_pixel);
-  int index = 0;
 
-  for (int i = 0; i < total_pixel * 3; i += 3) {
-    red[index] = img->data[i + 0] * 0.299f;
-    green[index] = img->data[i + 1] * 0.587f;
-    blue[index] = img->data[i + 2] * 0.114f;
+  for (int i = 0; i < total_pixel; i++) {
+    red[i] = (img->R[i] * 0.299f);
+    green[i] = (img->G[i] * 0.587f);
+    blue[i] = (img->B[i] * 0.114f);
+ 
+    gray_img->data[i] = (uint8_t)(red[i] + green[i] + blue[i]); 
+  } 
 
-    gray_img->data[index] = gray_pixel[index] =
-        (red[index] + green[index] + blue[index]);
-
-    index++;
-  }
-
-  free(red), free(green), free(blue), free(gray_pixel);
+  free(red), free(green), free(blue);
 
   return gray_img;
 }
